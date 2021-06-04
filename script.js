13bb78b2dbeb117f4c7f3e72c92577a1cfe9fcfc
IbrajKhan <ibraj.grd@gmail.com>
2021-06-04 14:54:24 +0530

    Getting the data for particular city
    and displaying  it to console

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let weather = {
   apiKey: "0beab466a1aaec66e02036ddf05b83db",
-  fetchWeather: function () {
+  fetchWeather: function (city) {
     fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=Denver&units=metric&appid=0beab466a1aaec66e02036ddf05b83db"
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { description, icon } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, description, icon, temp, humidity, speed);
   },
 };
