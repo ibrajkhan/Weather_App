7e831f390cf4754d429d0a02e97a35615b598a4b
IbrajKhan <ibraj.grd@gmail.com>
2021-06-04 19:40:59 +0530

    changing background image according to locations with unsplash

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ let weather = {
     document.querySelector(".wind").innerText =
       "Wind Speed: " + speed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
   },
   search: function () {
     this.fetchWeather(document.querySelector(".search_bar").value);
