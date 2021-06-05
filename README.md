0f1f607f890017653a5b719b2f60c045e516f36f
IbrajKhan <ibraj.grd@gmail.com>
2021-06-05 15:14:16 +0530

    Uses Geolocation api to get current location
    and for converting Geolocation latitude and longitude i uses opencagedata Api

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-you can see this project live at following URL: https://ibrajkhan.github.io/Weather_App/
+This is basically a weather app. It Automatically detect your current location and show weather report of your city.
+Here I uses openweathermap.org Api to get weather report and for detecting the current location i uses Geolocaion Api. I also used opencagedata Api to convert Geolocation latitude and longitutde to current city that you our are in.
+
+You can see this project live at following URL: https://ibrajkhan.github.io/Weather_App/
--- script.js
+++ script.js
@@ -21,9 +21,8 @@ let weather = {
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/w/" + icon + ".png";
     document.querySelector(".description").innerText = description;
-    document.querySelector(".humitity").innerText = document.querySelector(
-      ".humitity"
-    ).innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".humitity").innerText =
+      "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText =
       "Wind Speed: " + speed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
@@ -34,6 +33,59 @@ let weather = {
     this.fetchWeather(document.querySelector(".search_bar").value);
   },
 };
+let geocode = {
+  reverseGeocode: function (latitude, longitude) {
+    //Actually Geolocation api gives me latitude and longitude for my current location so i have to convert it into city. For this i uses opencagedata.api it simply convert latitude and longitude to current city that we are in.
+    var apikey = "a3af05d592ea43f3a5809b8bc6cd8edc";
+
+    var api_url = "https://api.opencagedata.com/geocode/v1/json";
+
+    var request_url =
+      api_url +
+      "?" +
+      "key=" +
+      apikey +
+      "&q=" +
+      encodeURIComponent(latitude + "," + longitude) +
+      "&pretty=1" +
+      "&no_annotations=1";
+
+    var request = new XMLHttpRequest();
+    request.open("GET", request_url, true);
+
+    request.onload = function () {
+      if (request.status === 200) {
+        // Success!
+        var data = JSON.parse(request.responseText);
+        weather.fetchWeather(data.results[0].components.city); // print the locations
+      } else if (request.status <= 500) {
+        console.log("unable to geocode! Response code: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log("error msg: " + data.status.message);
+      } else {
+        console.log("server error");
+      }
+    };
+
+    request.onerror = function () {
+      console.log("unable to connect to server");
+    };
+
+    request.send();
+  },
+
+  getLocation: function () {
+    function sucess(data) {
+      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(sucess, console.error);
+    } else {
+      weather.fetchWeather("New Delhi");
+    }
+  },
+};
+
 document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
 });
@@ -42,4 +94,4 @@ document.querySelector(".search_bar").addEventListener("keyup", function (e) {
     weather.search();
   }
 });
-weather.fetchWeather("New Delhi");
+geocode.getLocation();
